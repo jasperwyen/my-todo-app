0c7f88151ab184eb0229dee74168dcebb579e0a3
jasperwyen <jasper.wyen@gmail.com>
2022-10-08 17:54:33 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+
+    <v-main>
+      <v-avatar
+        color="primary"
+        size="99"
+      >ABC</v-avatar>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+  
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
